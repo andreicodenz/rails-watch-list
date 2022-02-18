3ebbc0c2089de1b78326cced8f17b30c070c8a52
andrei mori <andrei.code.nz@gmail.com>
2022-02-18 15:17:45 +0900

    fixed last re direction error

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
   def create
     @list = List.create(list_params)
     if @list.save
-      redirect_to lists_path(@list)
+      redirect_to list_path(@list)
     else
       render :new
     end
